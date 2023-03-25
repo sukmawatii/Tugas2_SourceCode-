d0b34096ff305a734cef193e46bfc16c3cd02e23
sukmawatii <127372014+sukmawatii@users.noreply.github.com>
2023-03-24 22:10:47 -0800

    Create PrintWhile1.java

---
+++ PrintWhile1.java
@@ -0,0 +1,30 @@
+/*Nama: Sukmawati*/
+/*NIM: 13020210025*/
+/*Hari/Tanggal: Sabtu/25-03-2023*/
+/*Waktu: 13.17 PM*/
+package tgs14;
+
+import java.util.Scanner;
+/* Baca N, */
+/* Print i = 1 s/d N dengan while (ringkas) */
+public class PrintWhile1 {
+/**
+* @param args
+*/
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+/* Kamus : */
+int N;
+int i = 1;
+Scanner masukan=new Scanner(System.in);
+/* Program */
+System.out.print ("Nilai N >0 = ");
+N = masukan.nextInt();
+System.out.print ("Print i dengan WHILE (ringkas): \n");
+while (i <= N){
+System.out.println (i++);
+} /* (i > N)*/
+
+}
+
+}
