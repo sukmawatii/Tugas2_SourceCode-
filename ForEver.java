35c0cfe08fd3af0205d30872617e7133125f5567
sukmawatii <127372014+sukmawatii@users.noreply.github.com>
2023-03-24 22:00:28 -0800

    Create ForEver.java

---
+++ ForEver.java
@@ -0,0 +1,22 @@
+/*Nama: Sukmawati*/
+/*NIM: 13020210025*/
+/*Hari/Tanggal: Sabtu/25-03-2023*/
+/*Waktu: 12.46 PM*/
+package tgs2;
+
+public class ForEver {
+
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Program */
+        System.out.println("Program akan looping, akhiri dengan ^c");
+        while (true){
+            System.out.print ("Print satu baris ....\n");
+        }
+    
+    }
+    
+}
