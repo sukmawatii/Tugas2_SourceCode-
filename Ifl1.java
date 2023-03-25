0f6a1882091f1014d8bf793939e59226328773cf
sukmawatii <127372014+sukmawatii@users.noreply.github.com>
2023-03-24 22:01:30 -0800

    Create Ifl1.java

---
+++ Ifl1.java
@@ -0,0 +1,31 @@
+/*Nama: Sukmawati*/
+/*NIM: 13020210025*/
+/*Hari/Tanggal: Sabtu/25-03-2023*/
+/*Waktu: 12.52 PM*/
+package tgs3;
+/* contoh pemakaian IF satu kasus */
+/* membaca nilai integer, menuliskan nilainya jika positif */
+
+import java.util.Scanner;
+
+public class If1 {
+
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus */
+        Scanner masukan=new Scanner(System.in);
+        int a;
+        /* Program */
+
+        System.out.print ("Contoh IF satu kasus \n");
+        System.out.print ("Ketikkan suatu nilai integer : ");
+        a = masukan.nextInt();
+        if (a >= 0)
+        System.out.print ("\nNilai a positif "+ a);
+    
+    }
+    
+}
