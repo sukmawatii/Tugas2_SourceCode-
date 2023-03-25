73998141285401c648f6d675df60d87913cad6bf
sukmawatii <127372014+sukmawatii@users.noreply.github.com>
2023-03-24 22:03:52 -0800

    Create KasusBoolean.java

---
+++ KasusBoolean.java
@@ -0,0 +1,29 @@
+/*Nama: Sukmawati*/
+/*NIM: 13020210025*/
+/*Hari/Tanggal: Sabtu/25-03-2023*/
+/*Waktu: 13.03 PM*/
+package tgs6;
+
+/* Eksrpesi kondisional dengan boolean */
+public class KasusBoolean {
+
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus */
+        boolean bool;
+        /* Algoritma */
+        bool = true;
+        if(bool) {
+            System.out.print("true\n");
+        } else
+            System.out.print("false\n");
+        if(!bool) {
+            System.out.print("salah\n");
+        } else
+            System.out.print("benar\n");
+
+    } 
+}
