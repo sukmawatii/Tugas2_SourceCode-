fc6dfab0463d23b384614bcdc25f0985f164be6c
sukmawatii <127372014+sukmawatii@users.noreply.github.com>
2023-03-24 22:02:48 -0800

    Create If3.java

---
+++ If3.java
@@ -0,0 +1,36 @@
+/*Nama: Sukmawati*/
+/*NIM: 13020210025*/
+/*Hari/Tanggal: Sabtu/25-03-2023*/
+/*Waktu: 13.02 PM*/
+package tgs5;
+import java.util.Scanner;
+/* contoh pemakaian IF tiga kasus */ /* Membaca sebuah nilai, */
+/* menuliskan 'Nilai a positif , nilai a', jika a >0 */
+/* 'Nilai Nol , nilai a', jika a = 0 */
+/* 'Nilai a negatif , nilai a', jika a <0 */
+
+public class If3 {
+    
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus */
+        int a;
+        Scanner masukan=new Scanner(System.in);
+        /* Program */
+
+        System.out.print ("Contoh IF tiga kasus \n");
+        System.out.print ("Ketikkan suatu nilai integer :");
+        a=masukan.nextInt();
+        if (a > 0){
+            System.out.println ("Nilai a positif "+ a);
+        }else if (a == 0){
+            System.out.println ("Nilai Nol "+ a);
+        }else /* a > 0 */ {
+            System.out.println ("Nilai a negatif "+ a);
+        }
+
+    }
+}
