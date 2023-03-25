af8fd26535710370a85ef785c5d216e47eefaab1
sukmawatii <127372014+sukmawatii@users.noreply.github.com>
2023-03-24 22:13:30 -0800

    Create PrintXWhile.java

---
+++ PrintXWhile.java
@@ -0,0 +1,33 @@
+/*Nama: Sukmawati*/
+/*NIM: 13020210025*/
+/*Hari/Tanggal: Sabtu/25-03-2023*/
+/*Waktu: 13.17 PM*/
+package tgs17;
+import java.util.Scanner;
+
+/* contoh baca nilai x, */
+/* Jumlahkan nilai yang dibaca dengan WHILE */
+public class PrintXWhile {
+    
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus : */
+        int Sum;
+        int x;
+        Scanner masukan=new Scanner(System.in);
+        /* Program */
+        Sum = 0;    /* Inisialisasi */
+        System.out.print ("Masukkan nilai x (int), akhiri dg 999 : ");
+        x = masukan.nextInt();      /* First Elmt*/
+        while (x != 999)    /* Kondisi berhenti */ {
+            Sum = Sum + x; /* Proses */
+            System.out.print("Masukkan nilai x (int), akhiri dg 999 : ");
+            x = masukan.nextInt();  /* First Elmt*/
+        }
+        System.out.println("Hasil penjumlahan = "+ Sum);    /*Terminasi */
+        
+    }
+}
